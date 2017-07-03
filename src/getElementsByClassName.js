7c7bacce4fd2794292472faeba6380bdfa88f792
Hack Reactor Student <student@hackreactor.com>
2017-07-03 14:52:29 -0700

    Add functionality for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,30 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  var doc;
+  var element = [];
+  var nodeArray = [];
+  if (arguments.length === 1){
+    doc = document.body;
+  } else {
+    doc = arguments[1];
+  }
+  if (doc.classList){
+    if (doc.classList.contains(className)){
+      element.push(doc);
+      nodeArray = nodeArray.concat(element);
+    } 
+  } 
+  if (doc.hasChildNodes()){
+    var children = doc.childNodes;
+    for (var i = 0; i<children.length; i++){
+      element = getElementsByClassName(className, children[i]);     
+      nodeArray = nodeArray.concat(element);
+    }
+  } 
+  if (nodeArray === []){
+    return;
+  } else {
+    return  nodeArray;
+  }
 };
