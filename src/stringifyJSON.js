920ac7a2aad578b4044cf830befe0e9742c2dc80
Kelly Whiting <kwhiting@gmail.com>
2017-07-03 11:44:05 -0700

    getting started

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,24 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // created cases for numbers, null, boolean, strings, arrays, objects
+  // numbers - add .toString()
+  var typeObj = typeof obj;
+  if (typeObj === 'string'){
+    return '"' + obj + '"';
+  } else if (typeObj === 'number' || typeObj === 'boolean'){
+    return obj.toString();
+  } else if (obj === null){
+    return 'null';
+  } else if (Array.isArray(obj)) {
+    var result = [];
+    for (var i = 0; i<obj.length; i++){
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result.join(',') + ']';
+  } else if (typeObj === 'function' || obj === undefined) {
+    // return nothing
+  } else {
+
+  }
 };
